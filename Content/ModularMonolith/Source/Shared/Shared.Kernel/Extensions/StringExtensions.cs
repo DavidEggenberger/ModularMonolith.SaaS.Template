f21df55e1a64b9a7a1c9c4e17a39dc1bcfd3f504
DavidEggenberger <david99.eggenberger@gmail.com>
2023-05-20 19:56:09 +0200

    Add Source/Test folders

---
+++ Content/ModularMonolith/Source/Shared/Shared.Kernel/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Kernel.Extensions
+{
+    public static class StringExtensions
+    {
+        public static Guid ToGuid(this string stringGuid)
+        {
+            return new Guid(stringGuid);
+        }
+    }
+}
---
+++ Content/ModularMonolith/Source/Shared/Shared.Server/AuthenticatedBaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Shared.Server
+{
+    public class AuthenticatedBaseController : ControllerBase
+    {
+        public async Task ActionResult()
+        {
+            
+        }
+    }
+}
