5a74220feae34dc15af1e5c66ed7a3be75751eb9
DavidEggenberger <david99.eggenberger@gmail.com>
2023-06-10 07:25:32 +0200

    shared styles

---
+++ Content/Source/Shared/Web/Shared.Web.Styles/ContentConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Web.Styles
+{
+    public class ContentConstants
+    {
+        public const string ContentPath = $"/_content/Shared.Web.Styles";
+        public const string BaseStyles = $"/_content/Shared.Web.Styles/BaseStyles.css";
+
+    }
+}
