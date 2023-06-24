cdab3340b89e32b635e2321b3b61a7957be14311
DavidEggenberger <david99.eggenberger@gmail.com>
2023-06-24 23:33:54 +0200

    fix registrating subscriptionconfig

--- Content/Source/Modules/Subscription/Modules.Subscription.DomainFeatures/Configuration/Registrator.cs
+++ Content/Source/Modules/Subscription/Modules.Subscription.DomainFeatures/Configuration/Registrator.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Modules.Subscription.DomainFeatures.Configuration
 {
