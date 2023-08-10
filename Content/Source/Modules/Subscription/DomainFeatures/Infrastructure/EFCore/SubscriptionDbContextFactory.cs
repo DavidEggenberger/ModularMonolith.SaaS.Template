38cabbcdf201c8bb2d0e5c6b460a2714bbf74473
DavidEggenberger <david99.eggenberger@gmail.com>
2023-08-10 22:57:23 +0200

    dbContext factories

---
+++ Content/Source/Modules/Subscription/DomainFeatures/Infrastructure/EFCore/SubscriptionDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Modules.Subscription.DomainFeatures.StripeSubscriptionAggregate.Domain;
+using Shared.Infrastructure.EFCore;
+using Shared.Infrastructure.EFCore.Configuration;
+using Shared.Infrastructure.MultiTenancy.EFCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modules.Subscription.DomainFeatures.Infrastructure.EFCore
+{
+    public class SubscriptionDbContextFactory : IDesignTimeDbContextFactory<SubscriptionDbContext>
+    {
+        public SubscriptionDbContext CreateDbContext(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Content/Source/Modules/TenantIdentity/DomainFeatures/Infrastructure/EFCore/TenantIdentityDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modules.TenantIdentity.DomainFeatures.Infrastructure.EFCore
+{
+    public class TenantIdentityDbContextFactory : IDesignTimeDbContextFactory<TenantIdentityDbContext>
+    {
+
+        public TenantIdentityDbContextFactory()
+        { 
+        }
+
+        public TenantIdentityDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<TenantIdentityDbContext>();
+            optionsBuilder.UseSqlServer();
+
+            return new TenantIdentityDbContext(optionsBuilder.Options);
+        }
+    }
+}
