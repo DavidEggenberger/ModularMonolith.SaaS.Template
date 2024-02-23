dc4ccdbc37b1121d80834ea60970a17503f39e12
DavidEggenberger <david99.eggenberger@gmail.com>
2024-02-23 23:50:18 +0100

    re-adding DomainEvent infrastructure

---
+++ Source/Shared/Features/CQRS/DomainEvent/IDomainEventDispatcher.cs
@@ -0,0 +1,9 @@
+﻿using Shared.Features.Domain;
+
+namespace Shared.Features.CQRS.DomainEvent
+{
+    public interface IDomainEventDispatcher
+    {
+        Task RaiseAsync<TDomainEvent>(TDomainEvent command, CancellationToken cancellation) where TDomainEvent : IDomainEvent;
+    }
+}
---
+++ Source/Shared/Features/CQRS/DomainEvent/IDomainEventHandler.cs
@@ -0,0 +1,9 @@
+﻿using Shared.Features.Domain;
+
+namespace Shared.Features.CQRS.DomainEvent
+{
+    public interface IDomainEventHandler<in TDomainEvent> where TDomainEvent : IDomainEvent
+    {
+        Task HandleAsync(TDomainEvent query, CancellationToken cancellation);
+    }
+}
