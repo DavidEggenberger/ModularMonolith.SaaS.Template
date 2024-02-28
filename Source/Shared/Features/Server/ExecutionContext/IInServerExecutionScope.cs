3969b8129f7ae4bbff531bb8c3cc4a855f5840b3
DavidEggenberger <david99.eggenberger@gmail.com>
2024-02-28 22:22:31 +0100

    IInServerExecutionScope

---
+++ Source/Shared/Features/Server/ExecutionContext/IInServerExecutionScope.cs
@@ -0,0 +1,9 @@
+﻿using Shared.Kernel.BuildingBlocks;
+
+namespace Shared.Features.Server.ExecutionContext
+{
+    public interface IInServerExecutionScope
+    {
+        public IExecutionContext ExecutionContext { get; init; }
+    }
+}
