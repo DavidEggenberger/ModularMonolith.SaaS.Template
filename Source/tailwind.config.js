8365d8a57f921c6833351cd229a95b2d18726110
DavidEggenberger <david99.eggenberger@gmail.com>
2024-05-28 21:09:31 +0200

    site css

--- Source/tailwind.config.js
+++ Source/tailwind.config.js
@@ -1,22 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{razor,html,cshtml}"],
-  theme: {
-      extend: {
-          gridTemplateRows: {
-              '40px-12rows': '40px repeat(12, minmax(0, 1fr)) ',
-              '50px-12rows': '50px repeat(12, minmax(0, 1fr)) ',
-              '80px-12rows': '80px repeat(12, minmax(0, 1fr)) ',
-          },
-      },
-      screens: {
-          'height-xsm': { 'raw': '(min-height: 400px)' },
-          'height-sm': { 'raw': '(min-height: 520px)' },
-          'height-md': { 'raw': '(min-height: 640px)' },
-          'height-lg': { 'raw': '(min-height: 800px)' },
-          'height-xl': { 'raw': '(min-height: 1000px)' },
-          'height-2xl': { 'raw': '(min-height: 1300px)' },
-
+    content: ["./**/*.{razor,html,cshtml}"],
+    theme: {
+        colors: {
+            'white': '#FFFFFF',
+            'grey-50': '#eff1f3',
+            'grey-100': '#edeef1',
+            'grey-200': 'rgb(229 231 235)',
+            'grey-300': 'rgb(209 213 219)',
+            'blue-50': '#f0f9ff',
+            'blue-100': '#e0f2fe',
+            'blue-200': '#bfdbfe',
+            'blue-300': '#7dd3fc',
+            'blue-400': '#38bdf8',
+            'blue-500': '#0ea5e9',
+            'blue-600': '#0284c7',
+            'blue-700': '#0369a1'
+        },
+     screens: {
           'width-xxsm': { 'raw': '(max-width: 300px)' },
           'width-xsm': { 'raw': '(mim-width: 480px)' },
           'width-sm': { 'raw': '(min-width: 560px)' },
@@ -24,7 +25,7 @@ module.exports = {
           'width-lg': { 'raw': '(min-width: 1024px)' },
           'width-xl': { 'raw': '(min-width: 1280px)' },
           'width-2xl': { 'raw': '(min-width: 1536px)' },
-      }
+     }
   },
   plugins: [],
 }
