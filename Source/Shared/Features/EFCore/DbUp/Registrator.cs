65dee642d12731421595c4da072c8d505e3be3b2
David Eggenberger <David.Eggenberger@hotmail.com>
2024-09-22 21:04:30 +0200

    create db if not exists

--- Source/Shared/Features/EFCore/DbUp/Registrator.cs
+++ Source/Shared/Features/EFCore/DbUp/Registrator.cs
@@ -1,4 +1,5 @@
 ﻿using DbUp;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Shared.Features.EFCore.Configuration;
@@ -9,19 +10,35 @@ namespace Shared.Features.EFCore.DbUp
 {
     public static class Registrator
     {
-        public static IServiceCollection AddDbUpMigration(this IServiceCollection services)
+        public static async Task<IServiceCollection> AddDbUpMigration(this IServiceCollection services)
         {
             using var serviceProvider = services.BuildServiceProvider();
 
             var isProduction = serviceProvider.GetRequiredService<IExecutionContext>().HostingEnvironment.IsProduction();
             var efCoreConfiguration = serviceProvider.GetRequiredService<EFCoreConfiguration>();
+            var connectionString = isProduction ? efCoreConfiguration.SQLServerConnectionString_Prod : efCoreConfiguration.SQLServerConnectionString_Dev;
 
             var upgrader = DeployChanges.To
-                               .SqlDatabase(isProduction ? efCoreConfiguration.SQLServerConnectionString_Prod : efCoreConfiguration.SQLServerConnectionString_Dev)
+                               .SqlDatabase(connectionString)
                                .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
-                               .JournalToSqlTable("dbo", "DbUP_MigrationHistory")
+                               .JournalToSqlTable("dbo", "MigrationHistory_DbUP")
                                .LogToConsole()
                                .Build();
+
+            if (upgrader.TryConnect(out string _) is false)
+            {
+                var sqlConnectionParameters = connectionString.Split(';');
+                var databaseName = sqlConnectionParameters[1].Split("=")[1];
+                var serverConnectionString = string.Join(";", sqlConnectionParameters.Where(e => e.StartsWith("Database") is false));
+
+                using (var connection = new SqlConnection(string.Join("", serverConnectionString)))
+                {
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandText = $"CREATE DATABASE {databaseName}";
+                    await command.ExecuteNonQueryAsync();
+                }
+            }
 
             var scripts = upgrader.GetScriptsToExecute();
             foreach (var script in scripts)
